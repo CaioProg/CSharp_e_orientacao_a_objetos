302618136345922d1afefd02379d972efebb69f8
CaioProg <caioprog18@gmail.com>
2022-03-05 17:45:24 -0300

    Primerio projeto no Visual Studio

---
+++ AprendendoCSharp/1-PrimeiroProjeto/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace _1_PrimeiroPorjeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Olá, mundo! Projeto no Visual Studio!");
+            Console.WriteLine("A execução acabou. Tecle enter para finalziar...");
+            Console.ReadLine();
+        }
+    }
+}
