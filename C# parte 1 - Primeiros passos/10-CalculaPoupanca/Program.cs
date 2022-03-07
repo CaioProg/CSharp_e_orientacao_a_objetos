7161b742ab284dea7c4ddb76d4d8020d171562a2
CaioProg <caioprog18@gmail.com>
2022-03-07 17:43:20 -0300

    Laço de repetição While

---
+++ C# parte 1 - Primeiros passos/10-CalculaPoupanca/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Execuando o projeto 10 - Calcula Poupança");
+
+        // 0.76% = 0,0076
+
+        double valorInvestido = 200000;
+        int contadorMes = 1;
+
+        while (contadorMes <= 12)
+            {
+            valorInvestido = valorInvestido + valorInvestido * 0.0076;
+            Console.WriteLine("Após " + contadorMes + " meses, você terá " + valorInvestido);
+
+            // contadorMes = contadorMes + 1;
+            // contadorMes += 1;
+            contadorMes++;
+        }
+        
+
+        
+        
+
+
+        Console.ReadLine();
+    }
+}
