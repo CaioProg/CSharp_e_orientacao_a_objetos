9784391a000978736d305eebe5c0027dca9264fd
CaioProg <caioprog18@gmail.com>
2022-03-07 19:00:40 -0300

    Desafio - Múltiplos de 3

---
+++ C# parte 1 - Primeiros passos/MultiplosDe3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Desafio Multiplo de 3");
+
+        for(int mult = 1; mult <= 100; mult++)
+        {
+            if (mult % 3 == 0)
+            {
+                Console.WriteLine(mult);
+            }
+        }
+    }
+}
