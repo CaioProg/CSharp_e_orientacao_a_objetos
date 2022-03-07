8869469f2603e478513f2aa01ec0dc28f6b5819a
CaioProg <caioprog18@gmail.com>
2022-03-07 18:18:44 -0300

    Transformando while em For

---
+++ C# parte 1 - Primeiros passos/TransformandoWhileEmFor/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(String[] args)
+    {
+        
+        for (int contador = 1; contador <= 100; contador++)
+        {
+            Console.WriteLine(contador);
+        }
+    }
+}
