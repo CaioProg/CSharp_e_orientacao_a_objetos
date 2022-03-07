90b164baeae37df8ba0cc2d5dadda683ee7e9168
CaioProg <caioprog18@gmail.com>
2022-03-07 18:38:43 -0300

    Um erro de compilação

---
+++ C# parte 1 - Primeiros passos/ErroNaCompilacao/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(String[] args)
+    {
+        int contador = 1;
+        while (contador <= 10)
+        {
+            Console.WriteLine(contador);
+            contador++;
+        }
+    }
+}
