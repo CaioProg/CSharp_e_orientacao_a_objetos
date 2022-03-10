d01947952551bfc459792f7d32c18f4553e5bbce
CaioProg <caioprog18@gmail.com>
2022-03-10 18:41:26 -0300

    Referências nulas

--- ByteBank/05-ByteBank/Program.cs
+++ ByteBank/05-ByteBank/Program.cs
@@ -10,23 +10,34 @@ namespace _05_ByteBank
     {
         static void Main(string[] args)
         {
-            Cliente gabriela = new Cliente();
+            // Cliente gabriela = new Cliente();
 
-            gabriela.nome = "Gabriela";
-            gabriela.profissao = "Desenvolvedora C#";
-            gabriela.cpf = "123.456.789-12";
+            // gabriela.nome = "Gabriela";
+            // gabriela.profissao = "Desenvolvedora C#";
+            // gabriela.cpf = "123.456.789-12";
 
             ContaCorrente conta = new ContaCorrente();
 
-            conta.titular = gabriela;
+            // conta.titular = gabriela;
+            // conta.titular = new Cliente();
+            // conta.titular.nome = "Gabriela Costa";
+            // conta.titular.cpf = "123.456.789-12";
+            // conta.titular.profissao = "Desenvolvedora C#";
+
             conta.saldo = 500;
             conta.agencia = 563;
             conta.numero = 545446;
 
-            conta.titular.nome = "Gabriela Costa";
+            if(conta.titular == null)
+            {
+                Console.WriteLine("Ops, a referência em  conta.titular é NULL");
+            }
 
-            Console.WriteLine(gabriela.nome);
-            Console.WriteLine(conta.titular.nome);
+            //Console.WriteLine(gabriela.nome);
+            Console.WriteLine(conta.titular);
+            // Console.WriteLine(conta.titular.nome);
+            // Console.WriteLine(conta.titular.cpf);
+            // Console.WriteLine(conta.titular.profissao);
 
 
             Console.ReadLine();
