09a15d41515d496600fb9fdeab6ef4f5430be3bd
CaioProg <caioprog18@gmail.com>
2022-03-10 20:22:41 -0300

    Construtores

--- ByteBank/07-ByteBank/ContaCorrente.cs
+++ ByteBank/07-ByteBank/ContaCorrente.cs
@@ -1,13 +1,33 @@
 ﻿
 
 
-namespace _06_ByteBank
+namespace _07_ByteBank
 {
     public class ContaCorrente
     {
-        
+
         public Cliente Titular { get; set; }
-        public int Agencia { get; set; }  
+
+        private int _agencia;
+        public int Agencia
+        {
+            get
+            {
+                return _agencia;
+            }
+            set
+            {
+                if (value <= 0)
+                {
+                    return;
+                }
+                _agencia = value;
+
+
+
+
+            }
+        }
         public int Numero { get; set; }
 
         private double _saldo = 100;
@@ -27,6 +47,12 @@ namespace _06_ByteBank
 
                 _saldo = value;
             }
+        }
+
+        public ContaCorrente(int agencia, int numero)
+        {
+            Agencia = agencia;
+            Numero = numero;
         }
 
         public bool Sacar(double valor)
