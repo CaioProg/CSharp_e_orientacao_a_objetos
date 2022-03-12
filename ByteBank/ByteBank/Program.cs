201fe3c1cf89a89d5968e1c9215ab401ab2cada1
CaioProg <caioprog18@gmail.com>
2022-03-12 11:10:11 -0300

    Classe Diretor e Sobrecargas

--- ByteBank/ByteBank/Program.cs
+++ ByteBank/ByteBank/Program.cs
@@ -11,16 +11,33 @@ namespace ByteBank
     {
         static void Main(string[] args)
         {
-            Funcionario carlos = new Funcionario(1);
+            GerenciadorBonificacao gerenciador = new GerenciadorBonificacao();
 
+            Funcionario carlos = new Funcionario();
             carlos.Nome = "Carlos";
             carlos.CPF = "584.554.917-53";
-            carlos.Salario = 5000;
+            carlos.Salario = 2000;
+
+            gerenciador.Registrar(carlos);
+
+            Diretor roberta = new Diretor();
+            roberta.Nome = "Roberta";
+            roberta.CPF = "789.456.123-78";
+            roberta.Salario = 5000;
+
+            gerenciador.Registrar(roberta);
+
 
             Console.WriteLine(carlos.Nome);
             Console.WriteLine(carlos.GetBonificacao());
 
-            
+            Console.WriteLine(roberta.Nome);
+            Console.WriteLine(roberta.GetBonificacao());
+
+            Console.WriteLine("Total de bonificações: " + gerenciador.GetTotalBonificacao());
+
+            Console.ReadLine();
+
         }
     }
 }
