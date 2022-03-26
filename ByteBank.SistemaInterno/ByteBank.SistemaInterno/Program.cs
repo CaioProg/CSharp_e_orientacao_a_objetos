9d2e597f1e312fb8a342d5dd31fd032cd6b912a0
CaioProg <caioprog18@gmail.com>
2022-03-26 18:17:30 -0300

    REeferencias de DLL

---
+++ ByteBank.SistemaInterno/ByteBank.SistemaInterno/Program.cs
@@ -0,0 +1,25 @@
+﻿using ByteBank.Modelos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente conta = new ContaCorrente(458, 544545);
+
+            Console.WriteLine(conta.Saldo);
+
+            conta.Sacar(-10);
+            string nome = "Caio";
+
+            Console.ReadLine();
+        }
+    }
+}
