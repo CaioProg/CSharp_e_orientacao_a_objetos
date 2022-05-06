f4947fc1b323094c6af8620690b64fab7181042b
CaioProg <caioprog18@gmail.com>
2022-05-06 09:14:00 -0300

    Conhecendo array

--- ByteBankSystem/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBankSystem/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -14,170 +14,20 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Olá, mundo!");
-            Console.WriteLine(123);
-            Console.WriteLine(10.5);
-            Console.WriteLine(true);
+            // ARRAY de inteiros, com 5 posições
 
-            object conta = new ContaCorrente(456, 12212);
-            object desenvolvedor = new Desenvolvedor("245548545");
+            int[] idades = new int [5];
 
-            string contaToString = conta.ToString();
+            idades[0] = 15;
+            idades[1] = 19;
+            idades[2] = 18;
+            idades[3] = 25;
+            idades[4] = 30;
 
-            Console.WriteLine("Resultado: " + contaToString);
-            Console.WriteLine(conta);
-
-
-            Cliente carlos_1 = new Cliente();
-            carlos_1.Nome = "Carlos";
-            carlos_1.CPF = "456.456.456-85";
-            carlos_1.Profissao = "Designer";
-
-            Cliente carlos_2 = new Cliente();
-            carlos_2.Nome = "Carlos";
-            carlos_2.CPF = "456.456.456-85";
-            carlos_2.Profissao = "Designer";
-
-            ContaCorrente conta2 = new ContaCorrente(456, 5645455);
-
-            if(carlos_1.Equals(carlos_2))
-            {
-                Console.WriteLine("São iguais");
-            }
-            else
-            {
-                Console.WriteLine("Não são iguais");
-            }
+            Console.WriteLine(idades[1]);
 
 
             Console.ReadLine();
-        }
-
-        static void TestaString()
-        {
-            // Olá, meu nome é Caio e você pode entrar em contato comigo 
-            // através do número 9999-9999!
-
-            // Meu nome é Caio, me  ligue em 9999-9899
-
-            // string padrao = "[0123456789][0123456789][0123456789][0123456789][-][0123456789][0123456789][0123456789][0123456789]";
-            // string padrao = "[0-9][0-9][0-9][0-9][-][0-9][0-9][0-9][0-9]";
-            // string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
-            // string padrao = "[0-9]{4,5}[-]{0,1}[0-9]{4}";
-            // string padrao = "[0-9]{4,5}-{0,1}[0-9]{4}";
-            string padrao = "[0-9]{4,5}-?[0-9]{4}";
-
-            string textoDeTeste = "Meu nome é Caio, me  ligue em 89919--9899";
-
-            Match resultado = (Regex.Match(textoDeTeste, padrao));
-
-
-            Console.WriteLine(resultado.Value);
-            Console.ReadLine();
-
-
-            // Exercicio
-            //string padrao1 = "Sala [A-G][-]?[0123456789]{2}";
-
-            //Console.WriteLine(Regex.IsMatch("Sala G345", padrao1));
-            //Console.WriteLine(Regex.IsMatch("Sala J-001", padrao1));
-            //Console.WriteLine(Regex.IsMatch("Sala a004", padrao1));
-            //Console.WriteLine(Regex.IsMatch("Sala C004", padrao1));
-            //Console.WriteLine(Regex.IsMatch("Minha sala é a sala G34", padrao1));
-            //Console.WriteLine(Regex.IsMatch("Minha sala é a Sala G-34", padrao1));
-            Console.ReadLine();
-
-
-            string urlTeste = "https://www.bytebank.com/cambio";
-            int indiceByteBank = urlTeste.IndexOf("https://www.bytebank.com");
-
-            Console.WriteLine(urlTeste.StartsWith("https://www.bytebank.com"));
-            Console.WriteLine(urlTeste.EndsWith("cambio/"));
-
-            Console.WriteLine(urlTeste.Contains("ByteBank"));
-
-            Console.ReadLine();
-
-            // pagina?argumentos
-            // 012345678
-
-
-
-            string urlParametros = "http://www.bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&valor=1500";
-            ExtratorValorDeArgumentosURL extratorDeValores = new ExtratorValorDeArgumentosURL(urlParametros);
-
-            string valor = extratorDeValores.GetValor("moedaDestino");
-            Console.WriteLine("Valor de moedaDestino: " + valor);
-
-            string valorMoedaOrigem = extratorDeValores.GetValor("moedaOrigem");
-            Console.WriteLine("Valor de moedaOrigem: " + valorMoedaOrigem);
-
-            Console.WriteLine(extratorDeValores.GetValor("VALOR"));
-
-            Console.ReadLine();
-
-            // Testando ToLower/ToUpper
-            string mensagemOrigem = "PALAVRA";
-            string termoBusca = "ra";
-
-            Console.WriteLine(termoBusca.ToUpper());
-            Console.WriteLine(mensagemOrigem.ToLower());
-
-            // Testando Replace
-            termoBusca = termoBusca.Replace('r', 'R');
-            Console.WriteLine(termoBusca);
-
-            termoBusca = termoBusca.Replace('a', 'A');
-            Console.WriteLine(termoBusca);
-
-            Console.WriteLine(mensagemOrigem.IndexOf(termoBusca));
-            Console.ReadLine();
-
-
-            // Testando o método Remove
-            string testeRemocao = "primeiraParte&123456789";
-            int indiceEComercial = testeRemocao.IndexOf('&');
-            Console.WriteLine(testeRemocao.Remove(indiceEComercial, 4));
-            Console.ReadLine();
-
-
-            string palavra = "moedaOrigiem=moedaDestino&moedaDestino=dolar";
-            string nomeArgumento = "moedaDestino=";
-
-            int indice = palavra.IndexOf(nomeArgumento);
-            Console.WriteLine(indice);
-
-            Console.WriteLine("Tamanho da string nomeArgumento: " + nomeArgumento.Length);
-
-            Console.WriteLine(palavra);
-            Console.WriteLine(palavra.Substring(indice));
-            Console.WriteLine(palavra.Substring(indice + nomeArgumento.Length));
-            Console.ReadLine();
-
-
-
-
-            string textoVazio = "";
-            Console.WriteLine(String.IsNullOrEmpty(textoVazio));
-
-            Console.ReadLine();
-
-
-            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL(null);
-
-            string url = "pagina?moedaOrigiem=real&moedaDestino=dolar";
-
-            int indiceInterrogacao = url.IndexOf('?');
-
-            Console.WriteLine(indiceInterrogacao);
-
-            Console.WriteLine(url);
-            string argumentos = url.Substring(indiceInterrogacao + 1);
-            Console.WriteLine(argumentos);
-
-
-
-
         }
     }
 }
