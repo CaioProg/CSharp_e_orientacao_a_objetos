36c5791cf16a7b82c5d87a929fd971487ad3044a
CaioProg <caioprog18@gmail.com>
2022-05-08 11:50:26 -0300

    Iterando arrays e Length

--- ByteBankSystem/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBankSystem/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -17,20 +17,32 @@ namespace ByteBank.SistemaAgencia
             // ARRAY de inteiros, com 5 posições
 
             int[] idades = null;
-            idades =  new int[5];
+            idades =  new int[3];
 
             idades[0] = 15;
             idades[1] = 19;
             idades[2] = 18;
-            idades[3] = 25;
-            idades[4] = 30;
+            //idades[3] = 25;
+            //idades[4] = 30;
+            //idades[5] = 60;
 
-            int indice = 3;
-            int idadeNoIndice4 = idades[indice];
+            Console.WriteLine(idades.Length);  
 
-            Console.WriteLine(idadeNoIndice4);
+            int acumulador = 0;
 
+            for(int indice = 0; indice < idades.Length; indice++)
+            {
+                int idade = idades[indice];
 
+                Console.WriteLine($"Acessando o array idades no índice {indice}");
+                Console.WriteLine($"Valor de idades no [{indice}] = {idade}");
+
+                acumulador += idade;
+            }
+
+            int media = acumulador / idades.Length;
+
+            Console.WriteLine($"Média de idades = {media}");
 
             Console.ReadLine();
         }
