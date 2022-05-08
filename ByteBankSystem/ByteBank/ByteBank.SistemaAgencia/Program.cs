c93a4aede49d17dbd80761cf102dc708f2eaed25
CaioProg <caioprog18@gmail.com>
2022-05-08 12:03:27 -0300

    Inicializador de array

--- ByteBankSystem/ByteBank/ByteBank.SistemaAgencia/Program.cs
+++ ByteBankSystem/ByteBank/ByteBank.SistemaAgencia/Program.cs
@@ -14,10 +14,30 @@ namespace ByteBank.SistemaAgencia
     {
         static void Main(string[] args)
         {
+
+            ContaCorrente[] contas = new ContaCorrente[]
+            {
+                new ContaCorrente(874, 55844660),
+                new ContaCorrente(874, 45548845),
+                new ContaCorrente(874, 78984515)
+            };
+
+            for (int indice= 0; indice < contas.Length; indice++)
+            {
+                ContaCorrente contaAtual = contas[indice];
+                Console.WriteLine($"Conta {indice} {contaAtual.Numero}");
+            }
+
+            Console.ReadLine();
+        }
+
+        static void TestaArrayInt()
+        {
+
             // ARRAY de inteiros, com 5 posições
 
             int[] idades = null;
-            idades =  new int[3];
+            idades = new int[3];
 
             idades[0] = 15;
             idades[1] = 19;
@@ -26,11 +46,11 @@ namespace ByteBank.SistemaAgencia
             //idades[4] = 30;
             //idades[5] = 60;
 
-            Console.WriteLine(idades.Length);  
+            Console.WriteLine(idades.Length);
 
             int acumulador = 0;
 
-            for(int indice = 0; indice < idades.Length; indice++)
+            for (int indice = 0; indice < idades.Length; indice++)
             {
                 int idade = idades[indice];
 
@@ -43,8 +63,6 @@ namespace ByteBank.SistemaAgencia
             int media = acumulador / idades.Length;
 
             Console.WriteLine($"Média de idades = {media}");
-
-            Console.ReadLine();
         }
     }
 }
