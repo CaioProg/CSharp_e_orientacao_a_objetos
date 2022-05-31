00453a28f3aad3604fa7419c18e66310b99a6768
CaioProg <caioprog18@gmail.com>
2022-05-31 20:34:54 -0300

    Usando o StreamReader

--- csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
+++ csharppt9-aula0/ByteBankImportacaoExportacao/Program.cs
@@ -8,48 +8,25 @@ using System.IO; // IO = Input e Output
 
 namespace ByteBankImportacaoExportacao
 {
-    class Program
+    partial class Program
     {
         static void Main(string[] args)
         {
             var enderecoDoArquivo = "contas.txt";
 
-            using (var fluxoDoArquivo = new FileStream(enderecoDoArquivo, FileMode.Open))
-
+            using (var fluxoDeArquivo = new FileStream(enderecoDoArquivo, FileMode.Open, FileAccess.Read))
+            using (var leitor = new StreamReader(fluxoDeArquivo))
             {
-                var buffer = new byte[1024]; // 1 kb
-                var numeroDeBytesLidos = -1;
-
-                while (numeroDeBytesLidos != 0)
+                while (!leitor.EndOfStream)
                 {
-                    numeroDeBytesLidos = fluxoDoArquivo.Read(buffer, 0, 1024);
-
-                    Console.WriteLine($"Bytes lidos {numeroDeBytesLidos}");
-
-                    EscreverBuffer(buffer, numeroDeBytesLidos);
-
+                    var linha = leitor.ReadLine();
+                    Console.WriteLine(linha);
 
                 }
-
-                fluxoDoArquivo.Close();
             }
-
 
             Console.ReadLine();
         }
 
-        static void EscreverBuffer(byte[] buffer, int bytesLidos)
-        {
-            var utf8 = Encoding.Default;
-
-            var texto = utf8.GetString(buffer, 0, bytesLidos);
-            Console.Write(texto);
-
-            //foreach (var meubyte in buffer)
-            //{
-            //    Console.Write(meubyte);
-            //    Console.Write(" ");
-            //}
-        }
     }
 }
