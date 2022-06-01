c73f2f4dc423308f9b3654c4032ec78dd7cfc076
CaioProg <caioprog18@gmail.com>
2022-05-31 21:34:24 -0300

    Usando StreamWriter e CreateNew

--- csharppt9-aula0/ByteBankImportacaoExportacao/3_CriandoArquivo.cs
+++ csharppt9-aula0/ByteBankImportacaoExportacao/3_CriandoArquivo.cs
@@ -26,5 +26,17 @@ namespace ByteBankImportacaoExportacao
                 fluxoDeArquivo.Write(bytes, 0, bytes.Length);
             }
         }
+
+        static void CriarArquivoComWriter()
+        {
+            var caminhoNovoArquivo = "contasExportadas.csv";
+
+            using (var fluxoDeArquivo = new FileStream(caminhoNovoArquivo, FileMode.CreateNew))
+            using (var escritor = new StreamWriter(fluxoDeArquivo, Encoding.UTF8))
+            {
+                escritor.Write("456,3434,456.0,Pedro Silva");
+            }
+        }
+
     }
 }
